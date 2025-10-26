ff03b40fb1e8221e2bc134fdf7dbd652d412d385
pyp <bobdirmanner@gmail.com>
2025-10-26 10:03:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,17 @@ function giveUp() {
   if (!currentWord) return;
   const overlay = document.createElement("div");
   overlay.classList.add("correct-overlay");
-  overlay.textContent = `The word was: ${currentWord}`;
+
+  overlay.innerHTML = `
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="28" height="28" style="vertical-align: middle; margin-right: 10px;">
+      <path d="M9 17C9.85038 16.3697 10.8846 16 12 16C13.1154 16 14.1496 16.3697 15 17" stroke="#ffea00" stroke-width="1.5" stroke-linecap="round"></path>
+      <ellipse cx="15" cy="10.5" rx="1" ry="1.5" fill="#ffea00"></ellipse>
+      <ellipse cx="9" cy="10.5" rx="1" ry="1.5" fill="#ffea00"></ellipse>
+      <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="#ffea00" stroke-width="1.5"></path>
+    </svg>
+    The word was: ${currentWord}
+  `;
+
   document.body.appendChild(overlay);
   setTimeout(() => {
     document.body.removeChild(overlay);
