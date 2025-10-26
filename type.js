25a3092997f17ccbb5724b2e4c985cd6757a4362
ren <py@astrarune.com>
2025-10-26 12:23:44 +0000

    Create type.js

---
+++ type.js
@@ -0,0 +1,14 @@
+<script>
+  const typeSound = new Audio("type.wav");
+  typeSound.preload = "auto";
+
+  document.querySelectorAll("input[type='text'], textarea").forEach(el => {
+    el.addEventListener("keydown", (event) => {
+      if (event.key.length === 1) {
+        const soundClone = typeSound.cloneNode();
+        soundClone.playbackRate = 0.9 + Math.random() * 0.2;
+        soundClone.play().catch(() => {});
+      }
+    });
+  });
+</script>
